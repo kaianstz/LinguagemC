9c50a039892cf364c0f9e8b2149e3a4343188961
Kaian Santos <kaianstsz@gmail.com>
2026-05-14 09:59:50 +0000

    Exemplo 02 de operadores relacionais em C com conversao implicita e explicita.

---
+++ Operadores_Relacionais/Exemplo_2.c
@@ -0,0 +1,22 @@
+    #include <stdio.h>
+
+    int main() {
+        float numero1 = 10.2;
+        int numero2 = 10;
+
+        //Comparação Implicita
+        printf("numero1 > numero2: %d\n", numero1 > numero2);
+        printf("numero1 == numero2: %d\n", numero1 == numero2);
+
+        printf("----------------------------\n");
+
+        //Comparação Explicita(casting)
+        printf("numero1 > numero 2: %d\n", (int)numero1 > numero2);
+        printf("numero1 == numero2: %d\n", (int)numero1 == numero2);
+
+        return 0;
+
+
+
+
+    }
